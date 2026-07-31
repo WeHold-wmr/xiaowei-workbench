f3c65da0ece3fbfdef79cc7474b594b7fdf5e4d1
WeHold-wmr <2764336964@qq.com>
2026-07-31 10:10:29 +0800

    fix: 跨设备同步改用 jsonblob 免密钥后端，刷新 SW 缓存版本

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'xw-1785398007953';
+const CACHE = 'xw-1785463801974';
 const IGNORE_CACHE = ['xw-v10', 'xw-v9', 'xw-v8', 'xw-v7', 'xw-v6', 'xw-v5', 'xw-v4', 'xw-v3', 'xw-v2', 'xw-v1'];
 
 const PRECACHE = [
